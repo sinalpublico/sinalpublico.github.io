bcbdcf1a2195399cc85662b6f7a2d66f516cc0f7
SuperBad <maketuuber@gmail.com>
2024-06-28 21:40:40 -0300

    Update js.js

--- html/js.js
+++ html/js.js
@@ -1,4 +1,10 @@
 var script = document.createElement('script');
 script.setAttribute('data-cfasync', 'false');
+script.setAttribute('src', '//achcdn.com/script/suv4.js');
 script.setAttribute('data-adel', 'lwsu');
+script.setAttribute('cdnd', 'achcdn.com');
+script.setAttribute('zid', '7692102');
 document.head.appendChild(script);
+
+
+/* function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','https://www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}ChatbroLoader({encodedChatId: '58xKJ'});*/
