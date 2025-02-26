c92ac71920e720f69d511ed6efc457c9166cde06
oi meu chapa <maketuuber@gmail.com>
2025-02-26 10:26:01 -0300

    Add files via upload

---
+++ admin/js/customize-preview.js
@@ -0,0 +1,1 @@
+/** * Live-update changed settings in real time in the Customizer preview. */( function( $ ) {	var api = wp.customize;	// Logo.	api( 'toroflix_theme_options[logo]', function( value ) {		value.bind( function( to ) {            $('.custom-logo').attr('src', to);		} );	} );} )( jQuery );
