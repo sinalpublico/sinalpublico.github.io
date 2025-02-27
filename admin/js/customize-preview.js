47b15efd658b4707a5585152bf252f65679a17f7
oi meu chapa <maketuuber@gmail.com>
2025-02-27 11:35:13 -0300

    Delete admin directory

--- admin/js/customize-preview.js
+++
@@ -1,1 +0,0 @@
-/** * Live-update changed settings in real time in the Customizer preview. */( function( $ ) {	var api = wp.customize;	// Logo.	api( 'toroflix_theme_options[logo]', function( value ) {		value.bind( function( to ) {            $('.custom-logo').attr('src', to);		} );	} );} )( jQuery );
